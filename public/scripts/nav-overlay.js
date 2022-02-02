8dda3690a4f4a432d9fb82e148555923004d5d70
willianchu <willianchu@hotmail.com>
2022-02-02 09:28:59 -0800

    fixed layout misundertand after a mentor advice

--- public/scripts/nav-overlay.js
+++ public/scripts/nav-overlay.js
@@ -1,7 +1,9 @@
 function openNav() {
-  document.getElementById("myNav").style.height = "520px";
+  document.getElementById("myNav").style.height = "430px";
+  document.getElementById("main-header").style.height = "0px";
 }
 
 function closeNav() {
-  document.getElementById("myNav").style.height = "0%";
+  document.getElementById("myNav").style.height = "00%";
+  document.getElementById("main-header").style.height = "100%";
 }
