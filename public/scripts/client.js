86d3b6b68213bc70512dfc48fe017f5f29076a75
willianchu <willianchu@hotmail.com>
2022-02-03 04:42:47 -0800

    fixed icon hover, and img loading, format issues

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,53 +1,49 @@
+
 /* eslint-disable no-undef */
 // Test / driver code (temporary). Eventually will get this from the server.
 console.log('client.js is running');
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1000113959088
-  }
-];
-
-
-
 $(()=> {
+  const addListeners = () => {
+    $('i.fa.fa-retweet').hover(function() {
+      $(this).css("color", "darkgoldenrod");
+    }, function() {
+      $(this).css("color", "#545149");
+    });
+  
+    $('.fa-heart').hover(function() {
+      $(this).css("color", "darkgoldenrod");
+    }, function() {
+      $(this).css("color", "#545149");
+    });
+
+    $('.fa-flag').hover(function() {
+      $(this).css("color", "darkgoldenrod");
+    }, function() {
+      $(this).css("color", "#545149");
+    });
+  };
+
   const renderTweets = function(tweets) {
     for (const tweetIndex in tweets) { // loops through tweets
       const $tweet = createTweetElement(tweets[tweetIndex]); // calls createTweetElement for each tweet
       $('#tweets-container').append($tweet); // takes return value and appends it to the tweets container
     }
+    addListeners();
   };
 
   const createTweetElement = function(tweetData) {
     // let $tweet = /* Your code for creating the tweet element */
     
-    const $idImg = $('img').attr('src',`${tweetData.user.avatars}`); // child 1
+    const $idImg = $('<img>').addClass("id-img").attr('src',`${tweetData.user.avatars}`); // child 1
     const $name = $('<h3>').text(`${tweetData.user.name}`); // child 2
     const $preId = $('<pre>').append($idImg, $name); // parent header-id
 
     const $marker = $('<h4>').text(`${tweetData.user.handle}`); // child 3
     const $header = $('<header>').addClass('tweet-header').append($preId, $marker); 
     
-    const $content = $('<p>').addClass('tweet-content').text(`${tweetData.content.text}`); // child 4
+    const $content = $('<p>').addClass('tweet-content').text(`${tweetData.content.text}`);
+    const $hr = $('<hr>'); // child 4
     
     const iflag = $('<i>').addClass('fa fa-flag'); // child 7
     const iretweet = $('<i>').addClass('fa fa-retweet'); // child 8
@@ -63,7 +59,7 @@ $(()=> {
 
     const $tweet = $('<article>').addClass('tweet'); // parent tweet
 
-    $tweet.append($header, $content, $footer); // parent tweet
+    $tweet.append($header, $content, $hr, $footer); // parent tweet
 
     const $tweetsContainer = $('#tweets-container');
 
@@ -74,10 +70,39 @@ $(()=> {
 
   $('#new-tweet-form').on('submit', function(event) {
     event.preventDefault(); // prevents page from reloading
-    console.log( "the form has submmited");
+    console.log("the form has submmited");
+
+    const caracterCount = $('#char-count').html();
+
+    console.log(typeof caracterCount);
+    
+    if (parseFloat(caracterCount) < 0) {
+      alert('You have exceeded the character limit');
+      $('input.error:button').css('display', 'inline');
+    } else {
+      const data = $("#new-tweet-form").serialize(); // serializes the form data
+      console.log(data);
+      $.ajax({
+        type: 'POST',
+        url: '/tweets',
+        data: data
+      }).done(function(data) {
+        console.log('data sent to server');
+        console.log(data);
+
+      });
+    }
     
   });
 
-  renderTweets(data);
+  $.ajax({
+    url: '/tweets',
+    method: 'GET',
+  }).then((tweets) => {
+    renderTweets(tweets);
+
+  });
+
 
 });
+ 
--- public/scripts/tweet-hover-state.js
+++ public/scripts/tweet-hover-state.js
@@ -1,23 +1,31 @@
+/* eslint-disable no-undef */
+
 $(document).ready(function() {
   console.log('tweet-hover-state.js loaded');
-  $('.fa.fa-retweet').hover(function() {
-  $(this).css("color", "darkgoldenrod");
-  }, function(){
-  $(this).css("color", "#545149");
-  });
 
-  $('.fa.fa-heart').hover(function() {
-    $(this).css("color", "darkgoldenrod");
-    }, function(){
-    $(this).css("color", "#545149");
+  const addListeners = () => {
+    $('i.fa.fa-retweet').hover(function() {
+      $(this).css("color", "darkgoldenrod");
+    }, function() {
+      $(this).css("color", "#545149");
+    });
+  
+    $('.fa-heart').hover(function() {
+      $(this).css("color", "darkgoldenrod");
+    }, function() {
+      $(this).css("color", "#545149");
     });
 
-    $('.fa.fa-flag').hover(function() {
+    $('.fa-flag').hover(function() {
       $(this).css("color", "darkgoldenrod");
-      }, function(){
+    }, function() {
       $(this).css("color", "#545149");
-      });
+    });
+  };
 });
+
+
+
 
   
   
